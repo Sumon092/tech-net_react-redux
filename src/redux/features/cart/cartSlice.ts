fd1b92374890bc8f88f45dcbb2129a3a776206c9
Sumon <m.sumon@outlook.com>
2023-07-12 16:45:49 +0600

    total price added to cart

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -3,9 +3,11 @@ import { PayloadAction, createSlice } from '@reduxjs/toolkit';
 
 interface ICart {
   products: IProduct[];
+  total: number;
 }
 const initialState: ICart = {
   products: [],
+  total: 0,
 };
 const cartSlice = createSlice({
   name: 'cart',
@@ -20,6 +22,7 @@ const cartSlice = createSlice({
       } else {
         state.products.push({ ...action.payload, quantity: 1 });
       }
+      state.total += action.payload.price;
     },
     removeOneFromCart: (state, action: PayloadAction<IProduct>) => {
       const existing = state.products.find(
@@ -32,11 +35,13 @@ const cartSlice = createSlice({
           (product) => product._id !== action.payload._id
         );
       }
+      state.total -= action.payload.price;
     },
     removeFromCart: (state, action: PayloadAction<IProduct>) => {
       state.products = state.products.filter(
         (product) => product._id !== action.payload._id
       );
+      state.total -= action.payload.price * action.payload.quantity!;
     },
   },
 });
