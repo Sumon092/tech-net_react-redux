2d7d37915f531c0a4d2ddb004e5eea4966018e98
Sumon <m.sumon@outlook.com>
2023-07-12 16:29:57 +0600

    remove one from cart added

--- src/redux/features/cart/cartSlice.ts
+++ src/redux/features/cart/cartSlice.ts
@@ -21,6 +21,18 @@ const cartSlice = createSlice({
         state.products.push({ ...action.payload, quantity: 1 });
       }
     },
+    removeOneFromCart: (state, action: PayloadAction<IProduct>) => {
+      const existing = state.products.find(
+        (product) => product._id === action.payload._id
+      );
+      if (existing && existing.quantity! > 1) {
+        existing.quantity = existing.quantity! - 1;
+      } else {
+        state.products = state.products.filter(
+          (product) => product._id !== action.payload._id
+        );
+      }
+    },
     removeFromCart: (state, action: PayloadAction<IProduct>) => {
       state.products = state.products.filter(
         (product) => product._id !== action.payload._id
@@ -28,5 +40,6 @@ const cartSlice = createSlice({
     },
   },
 });
-export const { addToCart, removeFromCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, removeOneFromCart } =
+  cartSlice.actions;
 export default cartSlice.reducer;
